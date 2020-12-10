b164ec79b1ea4f187e71fd6e414113893f5f4930
yuno-sa <75403468+yuno-sa@users.noreply.github.com>
2020-12-10 12:06:47 +0900

    Update main.js

--- first/scripts/main.js
+++ first/scripts/main.js
@@ -1,6 +1,7 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
-
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
 function setUserName() {
   let myName = prompt('あなたの名前を入力してください。');
   localStorage.setItem('name', myName);
@@ -10,5 +11,4 @@ function setUserName() {
 myButton.onclick = function() {
   setUserName();
 }
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
+
