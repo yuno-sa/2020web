1a2fedf12ccaa0a468fd5ba5f8861853443293ac
yuno-sa <75403468+yuno-sa@users.noreply.github.com>
2020-12-10 12:00:21 +0900

    Update main.js

--- first/scripts/main.js
+++ first/scripts/main.js
@@ -7,21 +7,7 @@ function setUserName() {
   localStorage.setItem('name', myName);
   myHeading.textContent = 'Mozilla はすばらしいよ、' + myName;
 }
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  let storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla はすばらしいよ、' + storedName;
-}
+
 myButton.onclick = function() {
   setUserName();
-}
-function setUserName() {
-  let myName = prompt('Please enter your name.');
-  if(!myName || myName === null) {
-    setUserName();
-  } else {
-    localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
-  }
 }
